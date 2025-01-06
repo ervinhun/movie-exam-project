30d8c067b8a38a0f8b7d6bf437fc43581c8f51b7
ervinhun <127263111+ervinhun@users.noreply.github.com>
2025-01-06 15:56:08 +0100

    Initial commit - FXML

--- src/main/java/dk/easv/movieexamproject/MovieApplication.java
+++ src/main/java/dk/easv/movieexamproject/MovieApplication.java
@@ -11,8 +11,8 @@ public class MovieApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MovieApplication.class.getResource("movie-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Movies");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/dk/easv/movieexamproject/MovieController.java
+++ src/main/java/dk/easv/movieexamproject/MovieController.java
@@ -1,14 +1,43 @@
 package dk.easv.movieexamproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
 
-public class MovieController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MovieController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private VBox vBoxRating;
+    @FXML private RadioButton radio2;
+    @FXML private RadioButton radio3;
+    @FXML private RadioButton radio4;
+    @FXML private RadioButton radio5;
+    @FXML private RadioButton radio6;
+    @FXML private RadioButton radio7;
+    @FXML private RadioButton radio8;
+    @FXML private RadioButton radio9;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //CreateImdbScore score = new CreateImdbScore(vBoxRating);
+        createIMDBScore();
+    }
+
+    private void createIMDBScore() {
+        ToggleGroup group = new ToggleGroup();
+        radio2.setToggleGroup(group);
+        radio3.setToggleGroup(group);
+        radio4.setToggleGroup(group);
+        radio5.setToggleGroup(group);
+        radio6.setToggleGroup(group);
+        radio7.setToggleGroup(group);
+        radio8.setToggleGroup(group);
+        radio9.setToggleGroup(group);
     }
 }
