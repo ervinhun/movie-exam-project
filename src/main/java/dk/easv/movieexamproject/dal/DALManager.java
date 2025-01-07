218ebfb74b55025689b99856aa3c9d5e5104e067
OleksandrDarchyk <oledar01@easv365.dk>
2025-01-07 11:34:19 +0100

    create getConnection

--- src/main/java/dk/easv/movieexamproject/dal/DALManager.java
+++ src/main/java/dk/easv/movieexamproject/dal/DALManager.java
@@ -1,4 +1,20 @@
 package dk.easv.movieexamproject.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
 public class DALManager {
+    public Connection getConnection() throws SQLServerException {
+        SQLServerDataSource ds;
+        ds = new SQLServerDataSource();
+        ds.setDatabaseName("movieProject"); // make this unique as names are shared on server
+        ds.setUser("CSe2024b_e_22"); // Use your own username
+        ds.setPassword("CSe2024bE22!24"); // Use your own password
+        ds.setServerName("EASV-DB4");
+        ds.setPortNumber(1433);
+        ds.setTrustServerCertificate(true);
+        return ds.getConnection();
+    }
 }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module dk.easv.movieexamproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens dk.easv.movieexamproject to javafx.fxml;
-    exports dk.easv.movieexamproject;
-}
